b7ad58ad1fcd6ff22d638bd21b3eff8b6eb2e2e6
Observer46 <microplayerpl@gmail.com>
2022-10-30 22:31:52 +0100

    fixing minor implementation details

--- project_dao.py
+++ project_dao.py
@@ -21,10 +21,13 @@ class ProjectDao:
         return [self.read_metadata(project_id) for project_id in self.read_all_names()]
 
     def read_all_names(self) -> List[str]:
-        return [obj.object_name.split('/')[-1] for obj in minio_controller.list_bucket_content("projects/")]
+        return [obj.object_name.replace("projects/", '')[:-1]
+                for obj in minio_controller.list_bucket_content("projects/")
+                if obj.object_name != "projects/"]
 
     def save_or_update_metadata(self, metadata: ProjectMetadata) -> None:
-        minio_controller.put_json_file(metadata.__dict__, f"projects/{metadata.project_id}/metadata.json")
+        minio_controller.put_json_file(metadata.to_json_response().__dict__,
+                                       f"projects/{metadata.project_id}/metadata.json")
 
     def delete_project(self, project_id: ProjectID) -> None:
         minio_controller.delete_directory(f"projects/{project_id}/")
@@ -51,7 +54,10 @@ class ProjectDao:
         minio_controller.delete_directory(f"projects/{project_id}/modflow/{modflow_id}/")
 
     def add_weather_file(self, project_id: ProjectID, weather_id: WeatherID, weather_file: FileStorage) -> None:
-        minio_controller.put_file(weather_file, f"projects/{project_id}/weather/{weather_id}.csv")
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            path_to_weather_file = os.path.join(tmp_dir, weather_file.filename)
+            weather_file.save(path_to_weather_file)
+            minio_controller.put_file(path_to_weather_file, f"projects/{project_id}/weather/{weather_id}.csv")
 
     def delete_weather_file(self, project_id: ProjectID, weather_file_id: WeatherID) -> None:
         minio_controller.delete_file(f"projects/{project_id}/weather/{weather_file_id}.csv")
@@ -60,32 +66,36 @@ class ProjectDao:
                              project_id: ProjectID,
                              shape_id: ShapeID,
                              shape_mask: np.ndarray) -> None:
-        with tempfile.NamedTemporaryFile() as tmp:
-            np.save(tmp, shape_mask)
-            minio_controller.put_file(tmp.name, f"projects/{project_id}/shapes/{shape_id}.npy")
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            shape_path = os.path.join(tmp_dir, "shape.npy")
+            np.save(shape_path, shape_mask)
+            minio_controller.put_file(shape_path, f"projects/{project_id}/shapes/{shape_id}.npy")
 
     def get_rch_shapes(self, project_id: ProjectID):
         rch_shapes = {}
         rch_paths = [obj.object_name for obj in
-                     minio_controller.list_bucket_content(f"projects/{project_id}/rch-shapes/")]
+                     minio_controller.list_bucket_content(f"projects/{project_id}/rch_shapes/")]
         for rch_shape_bucket_path in rch_paths:
-            shape_id = rch_shape_bucket_path.split("/")[-1]
+            shape_id = rch_shape_bucket_path.split("/")[-1].split('.')[0]
             rch_shapes[shape_id] = self.get_shape(project_id, shape_id)
+        return rch_shapes
 
     def delete_rch_shapes(self, project_id: ProjectID):
-        minio_controller.delete_directory(f"projects/{project_id}/rch-shapes")
+        minio_controller.delete_directory(f"projects/{project_id}/rch_shapes")
 
     def get_shape(self, project_id: ProjectID, shape_id: ShapeID) -> np.ndarray:
-        with tempfile.NamedTemporaryFile() as tmp:
-            minio_controller.get_file(f"projects/{project_id}/shapes/{shape_id}.npy", tmp.name)
-            return np.load(tmp.name)
+        shape_bucket = "rch_shapes" if shape_id.startswith("rch_shape_") else "shapes"
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            shape_path = os.path.join(tmp_dir, "shape.npy")
+            minio_controller.get_file(f"projects/{project_id}/{shape_bucket}/{shape_id}.npy", shape_path)
+            return np.load(shape_path)
 
     def delete_shape(self, project_id: ProjectID, shape_id: ShapeID) -> None:
         minio_controller.delete_file(f"projects/{project_id}/shapes/{shape_id}.npy")
 
     def add_modflow_rch_shapes(self, project_id: ProjectID, rch_shapes: List[np.ndarray]):
         for i, mask in enumerate(rch_shapes):
-            shape_id = f"rch-shape-{i + 1}.npy"
+            shape_id = f"rch_shape_{i + 1}.npy"
             self.save_or_update_shape(project_id, shape_id, mask)
 
 
