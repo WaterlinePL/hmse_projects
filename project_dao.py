e9a04e317c767e2e6557d02dcd974445431e3bbe
Observer46 <microplayerpl@gmail.com>
2022-12-12 09:09:25 +0100

    update import paths

--- project_dao.py
+++ project_dao.py
@@ -1,194 +1,154 @@
 # Interface
+import json
+import logging
 import os
 import shutil
-from abc import ABC, abstractmethod
 from typing import List
 
 import numpy as np
-from flask import send_file
 from werkzeug.datastructures import FileStorage
 
-from hmse_simulations.hmse_projects.hmse_hydrological_models.modflow.modflow_metadata import ModflowMetadata
-from hmse_simulations.hmse_projects.project_metadata import ProjectMetadata
-from hmse_simulations.hmse_projects.typing_help import ProjectID, ModflowID, HydrusID, WeatherID, ShapeID
+from hmse_projects.hmse_hydrological_models.modflow.modflow_metadata import ModflowMetadata
+from hmse_projects.project_metadata import ProjectMetadata
+from hmse_projects.typing_help import ProjectID, ModflowID, HydrusID, WeatherID, ShapeID
+
+WORKSPACE_PATH = 'workspace'
+METADATA_FILENAME = 'metadata.json'
 
 
-class ProjectDao(ABC):
+class ProjectDao:
 
-    @abstractmethod
+    def __init__(self):
+        os.makedirs(WORKSPACE_PATH, exist_ok=True)
+
     def read_metadata(self, project_id: ProjectID) -> ProjectMetadata:
-        ...
+        with open(os.path.join(WORKSPACE_PATH, project_id, METADATA_FILENAME), 'r') as handle:
+            metadata = ProjectMetadata(**json.load(handle))
+            if isinstance(metadata.modflow_metadata, dict):
+                metadata.modflow_metadata = ModflowMetadata(**metadata.modflow_metadata)
+            return metadata
 
-    @abstractmethod
     def read_all_metadata(self) -> List[ProjectMetadata]:
-        ...
+        return [self.read_metadata(project_id) for project_id in self.read_all_names()]
 
-    @abstractmethod
     def read_all_names(self) -> List[str]:
-        ...
+        return os.listdir(WORKSPACE_PATH)
 
-    @abstractmethod
     def save_or_update_metadata(self, metadata: ProjectMetadata) -> None:
-        ...
+        project_path = os.path.join(WORKSPACE_PATH, metadata.project_id)
+        os.makedirs(project_path, exist_ok=True)
+        with open(os.path.join(project_path, METADATA_FILENAME), 'w') as handle:
+            json.dump(metadata.to_json_response(), handle, indent=2, default=lambda o: o.__dict__)
+            os.makedirs(os.path.join(ProjectDao.get_project_path(metadata.project_id), 'modflow'), exist_ok=True)
+            os.makedirs(os.path.join(ProjectDao.get_project_path(metadata.project_id), 'hydrus'), exist_ok=True)
+            os.makedirs(os.path.join(ProjectDao.get_project_path(metadata.project_id), 'weather'), exist_ok=True)
+            os.makedirs(os.path.join(ProjectDao.get_project_path(metadata.project_id), 'shapes'), exist_ok=True)
+            os.makedirs(os.path.join(ProjectDao.get_rch_shapes_path(metadata.project_id)), exist_ok=True)
 
-    @abstractmethod
     def delete_project(self, project_id: ProjectID) -> None:
-        ...
+        try:
+            shutil.rmtree(ProjectDao.get_project_path(project_id))
+        except OSError as e:
+            logging.error(f"Error occurred during deleting project {project_id}: {str(e)}")
 
-    @abstractmethod
-    def download_project(self, project_id: ProjectID) -> FileStorage:
-        ...
+    def download_project(self, project_id: ProjectID) -> str:
+        return shutil.make_archive(base_name=os.path.join(ProjectDao.get_project_path(project_id), project_id),
+                                   format='zip',
+                                   root_dir=ProjectDao.get_simulation_path(project_id))
 
-    @abstractmethod
     def add_hydrus_model(self, project_id: ProjectID,
                          hydrus_id: HydrusID,
                          validated_model_path: os.PathLike) -> None:
-        ...
+        hydrus_model_dir = ProjectDao.get_hydrus_model_path(project_id, hydrus_id)
+        os.makedirs(hydrus_model_dir)
+        for hydrus_file in os.listdir(validated_model_path):
+            validated_file_path = os.path.join(validated_model_path, hydrus_file)
+            shutil.move(validated_file_path, hydrus_model_dir)
 
-    @abstractmethod
     def add_modflow_model(self, project_id: ProjectID,
                           modflow_id: ModflowID,
                           validated_model_path: os.PathLike) -> None:
-        ...
-
-    @abstractmethod
-    def delete_hydrus_model(self, project_id: ProjectID, hydrus_id: HydrusID) -> None:
-        ...
-
-    @abstractmethod
-    def delete_modflow_model(self, project_id: ProjectID, modflow_id: ModflowID) -> None:
-        ...
-
-    @abstractmethod
-    def add_weather_file(self, project_id: ProjectID, weather_id: WeatherID, weather_file: FileStorage) -> None:
-        ...
-
-    @abstractmethod
-    def delete_weather_file(self, project_id: ProjectID, weather_file_id: WeatherID) -> None:
-        ...
-
-    @abstractmethod
-    def save_or_update_shape(self,
-                             project_id: ProjectID,
-                             shape_id: ShapeID,
-                             shape_mask: np.ndarray) -> None:
-        ...
-
-    @abstractmethod
-    def get_rch_shapes(self, project_id: ProjectID):
-        ...
-
-    @abstractmethod
-    def delete_rch_shapes(self, project_id: ProjectID):
-        ...
-
-    @abstractmethod
-    def get_shape(self, project_id: ProjectID, shape_id: ShapeID) -> np.ndarray:
-        ...
-
-    @abstractmethod
-    def delete_shape(self, project_id: ProjectID, shape_id: ShapeID) -> None:
-        ...
-
-    @abstractmethod
-    def add_modflow_rch_shapes(self, project_id: ProjectID, rch_shapes: List[np.ndarray]):
-        ...
-
-
-class ProjectMock(ProjectDao):
-
-    def delete_rch_shapes(self, project_id: ProjectID):
-        pass
-
-    def add_modflow_rch_shapes(self, project_id: ProjectID, rch_shapes: List[np.ndarray]):
-        pass
-
-    def get_rch_shapes(self, project_id: ProjectID):
-        shape1 = np.zeros((10, 10))
-        shape1[7:10, :4] = 1
-        shape2 = np.zeros((10, 10))
-        shape2[:4, 7:10] = 1
-        return {
-            "rchShape1": shape1,
-            "rchShape2": shape2
-        }
-
-    def read_metadata(self, project_id: ProjectID) -> ProjectMetadata:
-        return ProjectMetadata(project_id,
-                               project_name=project_id,
-                               finished=True,
-                               lat=10.10,
-                               long=34.34,
-                               start_date='2020-01-01',
-                               spin_up=365,
-                               modflow_metadata=ModflowMetadata(
-                                   modflow_id="cekcyn-test",
-                                   rows=10,
-                                   cols=10,
-                                   grid_unit='meter',
-                                   row_cells=[50] * 10,
-                                   col_cells=[20] * 10,
-                                   duration=90
-                               ),
-                               hydrus_models={'las', 'trawa'},
-                               weather_files={'weather1', 'weather2'},
-                               shapes={'shape1': 'green',
-                                       'shape2': 'blue'},
-                               hydrus_to_weather={'las': 'weather1'},
-                               shapes_to_hydrus={'shape1': 'trawa'},
-                               hydrus_durations={'las': 365, 'trawa': 250},
-                               weather_files_durations={'weather1': 700, 'weather2': 800},
-                               )
-
-    def read_all_metadata(self) -> List[ProjectMetadata]:
-        return [ProjectMetadata("sample-project")]
-
-    def read_all_names(self) -> List[ProjectID]:
-        return ["sample-project"]
-
-    def save_or_update_metadata(self, metadata: ProjectMetadata) -> None:
-        pass
-
-    def delete_project(self, project_id: ProjectID) -> None:
-        pass
-
-    def download_project(self, project_id: ProjectID) -> FileStorage:
-        return "project.zip"
-
-    def add_hydrus_model(self, project_id: ProjectID, hydrus_id: ModflowID, archive: FileStorage) -> None:
-        pass
-
-    def add_modflow_model(self, project_id: ProjectID, modflow_id: ModflowID, archive: FileStorage) -> None:
-        pass
+        modflow_model_dir = ProjectDao.get_modflow_model_path(project_id, modflow_id)
+        os.makedirs(modflow_model_dir)
+        for modflow_file in os.listdir(validated_model_path):
+            validated_file_path = os.path.join(validated_model_path, modflow_file)
+            shutil.move(validated_file_path, modflow_model_dir)
 
     def delete_hydrus_model(self, project_id: ProjectID, hydrus_id: HydrusID) -> None:
-        pass
+        shutil.rmtree(ProjectDao.get_hydrus_model_path(project_id, hydrus_id))
 
     def delete_modflow_model(self, project_id: ProjectID, modflow_id: ModflowID) -> None:
-        pass
+        shutil.rmtree(ProjectDao.get_modflow_model_path(project_id, modflow_id))
+        rch_shapes_path = ProjectDao.get_rch_shapes_path(project_id)
+        shutil.rmtree(rch_shapes_path)
+        os.makedirs(rch_shapes_path)
 
     def add_weather_file(self, project_id: ProjectID, weather_id: WeatherID, weather_file: FileStorage) -> None:
-        pass
+        os.makedirs(os.path.join(ProjectDao.get_project_path(project_id), 'weather'), exist_ok=True)
+        weather_file.save(f"{ProjectDao.get_weather_model_path(project_id, weather_id)}")
 
     def delete_weather_file(self, project_id: ProjectID, weather_file_id: WeatherID) -> None:
-        pass
+        os.remove(ProjectDao.get_weather_model_path(project_id, weather_file_id))
 
     def save_or_update_shape(self,
                              project_id: ProjectID,
                              shape_id: ShapeID,
                              shape_mask: np.ndarray) -> None:
-        pass
+        os.makedirs(os.path.join(ProjectDao.get_project_path(project_id), 'shapes'), exist_ok=True)
+        np.save(ProjectDao.get_shape_path(project_id, shape_id), shape_mask)
+
+    def get_rch_shapes(self, project_id: ProjectID):
+        return {shape_id[:-4]: np.load(ProjectDao.get_rch_shape_filepath(project_id, shape_id[:-4]))
+                for shape_id in os.listdir(ProjectDao.get_rch_shapes_path(project_id))}
+
+    def delete_rch_shapes(self, project_id: ProjectID):
+        rch_shapes_path = ProjectDao.get_rch_shapes_path(project_id)
+        for rch_shape_id in os.listdir(rch_shapes_path):
+            os.remove(os.path.join(rch_shapes_path, rch_shape_id))
 
     def get_shape(self, project_id: ProjectID, shape_id: ShapeID) -> np.ndarray:
-        mask = np.zeros((10, 10))
-        if shape_id == 'shape1':
-            mask[:6, :6] = 1
-        elif shape_id == 'shape2':
-            mask[6:10, 6:10] = 1
-        return mask
+        return np.load(ProjectDao.get_shape_path(project_id, shape_id))
 
     def delete_shape(self, project_id: ProjectID, shape_id: ShapeID) -> None:
-        pass
+        os.remove(ProjectDao.get_shape_path(project_id, shape_id))
+
+    def add_modflow_rch_shapes(self, project_id: ProjectID, rch_shapes: List[np.ndarray]):
+        os.makedirs(os.path.join(ProjectDao.get_project_path(project_id), 'rch_shapes'), exist_ok=True)
+        for i, mask in enumerate(rch_shapes):
+            shape_id = f"rch_shape_{i + 1}"
+            np.save(ProjectDao.get_rch_shape_filepath(project_id, shape_id), mask)
+
+    @staticmethod
+    def get_project_path(project_id: ProjectID):
+        return os.path.join(WORKSPACE_PATH, project_id)
+
+    @staticmethod
+    def get_modflow_model_path(project_id: ProjectID, modflow_id: ModflowID):
+        return os.path.join(ProjectDao.get_project_path(project_id), 'modflow', modflow_id)
+
+    @staticmethod
+    def get_hydrus_model_path(project_id: ProjectID, hydrus_id: HydrusID):
+        return os.path.join(ProjectDao.get_project_path(project_id), 'hydrus', hydrus_id)
+
+    @staticmethod
+    def get_weather_model_path(project_id: ProjectID, weather_id: WeatherID):
+        return os.path.join(ProjectDao.get_project_path(project_id), 'weather', f"{weather_id}.csv")
+
+    @staticmethod
+    def get_shape_path(project_id: ProjectID, shape_id: ShapeID):
+        return os.path.join(ProjectDao.get_project_path(project_id), 'shapes', f"{shape_id}.npy")
+
+    @staticmethod
+    def get_rch_shapes_path(project_id: ProjectID):
+        return os.path.join(ProjectDao.get_project_path(project_id), 'rch_shapes')
+
+    @staticmethod
+    def get_rch_shape_filepath(project_id: ProjectID, shape_id: ShapeID):
+        return os.path.join(ProjectDao.get_rch_shapes_path(project_id), f"{shape_id}.npy")
+
+    @staticmethod
+    def get_simulation_path(project_id: ProjectID):
+        return os.path.join(ProjectDao.get_project_path(project_id), "simulation")
 
 
-project_dao = ProjectMock()
+project_dao = ProjectDao()
